72a929fe784fdfb6392722f0ccffc00531443410
Elang <aqsolelang@gmail.com>
2024-03-13 07:45:14 +0700

    fix: bug on customer page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,3 @@
-// import { Error404 } from "@/components/custom-components/error404";
-
 export default function Page() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <h1>This is The Dashboard Page</h1>
-    </div>
-  );
+  return <h1>This is The Dashboard Page</h1>;
 }
